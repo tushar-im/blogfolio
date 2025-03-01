877d82e01238c69d6d8a398df2163f483c3667b8
Tushar Sarang <tushar@theprocedure.in>
2025-03-01 15:59:00 +0300

    Fixes upgraded tailwind class and utils

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
-  site: "https://astro-nano-demo.vercel.app",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	integrations: [mdx(), sitemap()],
+	site: "https://tushar.im",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
